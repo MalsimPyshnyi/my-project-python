394ac21fb0fd48bde93248aac22078b9010e03b5
maksimpyshnyi <maksimpyshnyi@yandex-team.ru>
2020-07-31 21:08:02 +0300

    testtest1

--- srv.py
+++ srv.py
@@ -6,9 +6,10 @@ PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
 class MyHandler(SimpleHTTPRequestHandler):
-    def do_GET(self): #метод, в котором мы задаем условия
-        x = 1
+    def handle_root(self): #пищем функцию чтобы далее ее вызвать
+        pass
 
+    def handle_hello(self): #пишем также функцию чтобы во втором if ее вызвать
         content = f"""
         <html>
         <head>
@@ -23,10 +24,23 @@ class MyHandler(SimpleHTTPRequestHandler):
         """
 
         self.send_response(200)
-        self.send_header("Content-type", "text/html") #Заголовки = устанавливаем тип
-        self.send_header("Content-length", str(len(content))) #Заголовки = устанавливаем длину и задаем тип символы
-        self.end_headers() #обязательна пустая строкаа вставляется - разобраться
-        self.wfile.write(content.encode()) #Записываем файл
+        self.send_header("Content-type", "text/html")  # Заголовки = устанавливаем тип
+        self.send_header("Content-length", str(len(content)))  # Заголовки = устанавливаем длину и задаем тип символы
+        self.end_headers()  # обязательна пустая строкаа вставляется - разобраться
+        self.wfile.write(content.encode())  # Записываем файл
+
+    def handle_404(self):
+        pass
+
+    def do_GET(self): #метод, в котором мы задаем условия
+        if self.path == "/":
+            self.handle_root() #вызываем  функцию, что хотим показывать
+        elif self.path == "/hello": #elif значит, что образуется  цепочка и если  на первом iF выполниться, то  мы не выйдем, а если не выполнится - то выйдем из условия
+            self.handle_hello() #вызываем  функцию, что хотим показывать
+        else:
+            self.handle_404() #вызываем  функцию, что хотим показывать
+
+        x = 1
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
