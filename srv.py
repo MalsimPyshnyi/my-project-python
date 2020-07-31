a1b05bd7b1e05183b4eadc24b6d34847359b0ce7
maksimpyshnyi <maksimpyshnyi@yandex-team.ru>
2020-07-31 20:05:22 +0300

    testtest

--- srv.py
+++ srv.py
@@ -19,7 +19,8 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html") #Заголовки = устанавливаем тип
         self.send_header("Content-length", str(len(content))) #Заголовки = устанавливаем длину и задаем тип символы
-        self.wfile.write(conten) #Записываем файл
+        self.end_headers()
+        self.wfile.write(content.encode()) #Записываем файл
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
