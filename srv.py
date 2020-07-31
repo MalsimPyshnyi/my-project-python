d62f86a03e3d3f4bc5320d79def5b4e3cca18649
maksimpyshnyi <maksimpyshnyi@yandex-team.ru>
2020-07-31 19:59:20 +0300

    testtest

--- srv.py
+++ srv.py
@@ -1,10 +1,24 @@
 import os
 import socketserver
 from http.server import SimpleHTTPRequestHandler
+
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
+
 class MyHandler(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self): #метод, в котором мы задаем условия
+        content = """
+        <html>
+        <head><title>XXX</title></head>
+        <body>Hello world</body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.set_header("Content-type", "text/html") #Заголовки = устанавливаем тип
+        self.set_header("Content-length", str(len(content))) #Заголовки = устанавливаем длину и задаем тип символы
+        self.wfile.write(conten) #Записываем файл
+
 if __name__ == "__main__":
     with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
         print("it" + " works")
