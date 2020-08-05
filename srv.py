7de1a44279418beffc86ec64379379a89c535c09
maksimpyshnyi <maksimpyshnyi@yandex-team.ru>
2020-08-05 16:43:34 +0300

    test

--- srv.py
+++ srv.py
@@ -5,7 +5,19 @@ from http.server import SimpleHTTPRequestHandler
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
+CACHE_AGE = 60 * 60 * 24
+
 class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self): #метод, в котором мы задаем условия
+        path = self.build_path() # Тут не очень понял
+
+        if path == "/":
+            self.handle_root() #вызываем  функцию, что хотим показывать
+        elif path == "/hello/": #elif значит, что образуется  цепочка и если  на первом iF выполниться, то  мы не выйдем, а если не выполнится - то выйдем из условия
+            self.handle_hello() #вызываем  функцию, что хотим показывать
+        else:
+            self.handle_404() #вызываем  функцию, что хотим показывать
+
     def handle_root(self): #пищем функцию чтобы далее ее вызвать
         return super().do_GET() #обращаемся к родителю
 
@@ -22,29 +34,20 @@ class MyHandler(SimpleHTTPRequestHandler):
         </html>
         """
 
-        self.send_respond(content) #используем аргумень контент и значение оттуда подставляется
+        self.respond(content) #используем аргумень контент и значение оттуда подставляется
 
     def handle_404(self):
         msg = """NOT FOUND!!!!!!!"""
 
-        self.send_respond(msg, сode=404, content_type="text/plain")
+        self.respond(msg, сode=404, content_type="text/plain")
 
     def respond(self, message,code=200, content_type="text/html"):  #функция которая будет передаватьяс в 404, 200 и так длаее
         self.send_response(code)
         self.send_header("Content-type", content_type)
         self.send_header("Content-length", str(len(content)))
+        self.send_header("Cache-control", f"max-age={CACHE_AGE}")
         self.end_headers()
         self.wfile.write(message.encode())
-
-    def do_GET(self): #метод, в котором мы задаем условия
-        path = self.build_path() # Тут не очень понял
-
-        if self.path == "/":
-            self.handle_root() #вызываем  функцию, что хотим показывать
-        elif self.path == "/hello/": #elif значит, что образуется  цепочка и если  на первом iF выполниться, то  мы не выйдем, а если не выполнится - то выйдем из условия
-            self.handle_hello() #вызываем  функцию, что хотим показывать
-        else:
-            self.handle_404() #вызываем  функцию, что хотим показывать
 
     def build_path(self) -> str: #разобраться, -> подсказка типа - в данном случае возврат данных строка
         result = self.path #тут оператор присваивания, нужно понять что такое self.path
