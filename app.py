8471ac176a34dfee56853c95e55f66df2affe992
maksimpyshnyi <maksimpyshnyi@yandex-team.ru>
2020-09-16 21:13:28 +0300

    test12345

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import settings
 from server import MyHttp
 
 
-def server_greet():
-    print()
-    print("*" * 40, "run", "*" * 40)
-    print(f"PROJECT_DIR: \t{settings.PROJECT_DIR}")
-    print(f"SERVER:      \thttp://localhost:{settings.PORT}")
-    print("*" * 85)
-    print()
+# def server_greet():
+#     print()
+#     print("*" * 40, "run", "*" * 40)
+#     print(f"PROJECT_DIR: \t{settings.PROJECT_DIR}")
+#     print(f"SERVER:      \thttp://localhost:{settings.PORT}")
+#     print("*" * 85)
+#     print()
 
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", settings.PORT), MyHttp) as httpd:
-        server_greet()
+        # server_greet()
         try:
             httpd.serve_forever(poll_interval=0.4)
         finally:
